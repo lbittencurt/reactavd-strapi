7887ccb007557f4da073f3e2bf5a97004f20739f
Lucas Bittencurt <bittencurt.lucas@outlook.com>
2020-08-17 23:32:12 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '441908ebfdcb4b523576dc05ae1578da'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7dde95c5-ca2b-4bfc-a35b-f16cf56fac01'
+};
